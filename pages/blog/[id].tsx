59880c860664f084437620668d0b334be95780b8
katojun <jkky0225@gmail.com>
2022-06-26 19:11:35 +0900

    microCMS導入

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,40 @@
+import { Blog } from "..";
+import { client } from "../../lib/client";
+
+type Props = {
+  blog: Blog;
+};
+
+export const BlogId: React.FC<Props> = ({ blog }) => {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+  );
+};
+
+export default BlogId;
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content: Blog) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context: { params: Blog }) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
