d4e0dd4612e4304828230e177c5475ba1a3aef54
katojun <jkky0225@gmail.com>
2022-06-26 22:35:13 +0900

    ProfileCard

---
+++ components/card/ProfileCard.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import IconAndLink from "../icon/IconAndLink";
+import styles from "./profileCard.module.css";
+
+type Props = {
+  targetProfile: Pick<Profile, "image" | "familyName" | "lastName" | "role">;
+};
+
+export type Profile = {
+  familyName: string;
+  lastName: string;
+  role: string;
+  image: ImageType;
+  createdAt: number;
+  publishedAt: number;
+  rebisedAt: string;
+  updatedAt: string;
+};
+
+type ImageType = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+const ExternalLinks = {
+  Twitter: "https://twitter.com/test",
+  Github: "https://github.com/junichi4250",
+};
+
+const getFullName = (familyName: string, lastName: string): string => {
+  return `${familyName} ${lastName}`;
+};
+
+const ProfileCard: React.FC<Props> = ({ targetProfile }) => {
+  return (
+    <div>
+      <div
+        className={styles.image}
+        style={{ backgroundImage: `url(${targetProfile.image.url})` }}
+        role="img"
+      ></div>
+      <div className={styles.fullName}>
+        {getFullName(targetProfile.familyName, targetProfile.lastName)}
+      </div>
+      <div className={styles.role}>{targetProfile.role}</div>
+      <IconAndLink
+        iconType="GITHUB"
+        label={"Github"}
+        className={styles.marginTop}
+        externalLink={ExternalLinks.Github}
+      />
+    </div>
+  );
+};
+
+export default ProfileCard;
