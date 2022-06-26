ca411ec169e27fecb1243ca2a4ebd225fc656bde
katojun <jkky0225@gmail.com>
2022-06-26 22:03:58 +0900

    記事詳細ページ作成

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,13 +1,31 @@
 import { Blog } from "..";
+import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 import { client } from "../../lib/client";
+import { pagesPath } from "../../module/url/pagesPath";
+import { SiteUrl } from "../../module/url/siteUrl";
 
 type Props = {
   blog: Blog;
 };
 
 export const BlogId: React.FC<Props> = ({ blog }) => {
+  const breadcrumb: Breadcrumb[] = [
+    {
+      path: pagesPath.$url().pathname,
+      label: SiteUrl.Top.pageName,
+    },
+    {
+      path: pagesPath.$url().pathname,
+      label: SiteUrl.Top.pageName,
+    },
+    {
+      path: pagesPath.$url().pathname,
+      label: blog.title,
+    },
+  ];
   return (
     <main>
+      <Breadcrumb links={breadcrumb} />
       <h1>{blog.title}</h1>
       <p>{blog.publishedAt}</p>
       <p>{blog.category && `${blog.category.name}`}</p>
