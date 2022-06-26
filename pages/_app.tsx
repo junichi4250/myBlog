ebd8c740845c259ec7ea5189bf9f76668ecaa84c
katojun <jkky0225@gmail.com>
2022-06-26 22:04:38 +0900

    layout適用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layouts from "../components/layouts/Layouts";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  );
 }
 
-export default MyApp
+export default MyApp;
