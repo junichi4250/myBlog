9ac9afac36e22a2262896d0df21c1813aec35969
katojun <jkky0225@gmail.com>
2022-07-02 14:22:38 +0900

    footer作成

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -1,19 +1,33 @@
-import { makeStyles } from "@material-ui/styles";
+import Link from "next/link";
 import React from "react";
+import { pagesPath } from "../../../module/url/pagesPath";
+import styles from "./footer.module.scss";
 
-const useStyles = makeStyles({
-  footer: {
-    color: "#ebf6f7",
-    backgroundColor: "#0f2350",
-    width: "100%",
-    position: "absolute",
-    bottom: 0,
-  },
-});
-
-const Footer = () => {
-  const classes = useStyles();
-  return <div className={classes.footer}>Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.footerLinks}>
+        <li className={styles.footerLink}>
+          <Link href={pagesPath.disclaimer.$url().pathname}>
+            <a className={styles.link}>免責事項</a>
+          </Link>
+        </li>
+        <li className={styles.footerLink}>
+          <Link href={pagesPath.privacy.$url().pathname}>
+            <a className={styles.link}>プライバシーポリシー</a>
+          </Link>
+        </li>
+        <li className={styles.footerLink} onClick={() => undefined}>
+          <Link href={""}>
+            <a className={styles.link}>プロフィール</a>
+          </Link>
+        </li>
+      </ul>
+      <p className={styles.copyright}>
+        Copyright © Junichi Kato. All rights reserved
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
