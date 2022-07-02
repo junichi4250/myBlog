6a2e8da3b9a1f5e3f10eda35895e86d9a9efd13c
katojun <jkky0225@gmail.com>
2022-07-02 14:22:18 +0900

    header作成

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { AppBar, makeStyles } from "@material-ui/core";
+import Link from "next/link";
+import styles from "./header.module.scss";
+import { pagesPath } from "../../../module/url/pagesPath";
 
-const useStyles = makeStyles({
-  bar: {
-    padding: "20px",
-  },
-});
-
-const Header = () => {
-  const classes = useStyles();
-  return <AppBar>JunBlog</AppBar>;
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerInner}>
+        <div className={styles.left}>
+          <Link href={pagesPath.$url()}>
+            <a className={styles.logo}>jun blog</a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
